a7400f27bb0f18e3c0f79cdbc7f91ac9c85cd2ff
Zeppell108 <zeppell108@gmail.com>
2018-05-09 18:48:14 +0200

    update

--- js/cv.js
+++ js/cv.js
@@ -1,5 +1,14 @@
 $(function() {
 
+    // all dates center
+    $('td:nth-child(4n)').css('text-align', 'center');
+
+    // polish site after reload
+    $('.gb').each(function () {
+        $(this).css('display', 'none');
+    });
+
+    // change language of the site by clicking proper flag of country
     $('input').on("click", function () {
 
         var show;
@@ -15,6 +24,7 @@ $(function() {
         }
         $('.' + hide).each(function () {
             $(this).css('display', 'none');
+            // $('.' + show).removeAttr('style');
         });
         $('.' + show).each(function () {
             $(this).removeAttr('style');
